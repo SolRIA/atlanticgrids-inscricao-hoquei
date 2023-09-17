0175909c02fa1277886de724dbe833e0f7b39334
Frederico Regateiro <frederico.regateiro@solria.pt>
2023-09-17 11:15:18 +0100

    atualizada a mensagem de sucesso

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 A Quasar Project
 
 ## Install the dependencies
+
 ```bash
 yarn
 # or
@@ -10,32 +11,47 @@ npm install
 ```
 
 ### Start the app in development mode (hot-code reloading, error reporting, etc.)
+
 ```bash
 quasar dev
 ```
 
-
 ### Lint the files
+
 ```bash
 yarn lint
 # or
 npm run lint
 ```
 
-
 ### Format the files
+
 ```bash
 yarn format
 # or
 npm run format
 ```
 
-
-
 ### Build the app for production
+
 ```bash
 quasar build
 ```
 
 ### Customize the configuration
+
 See [Configuring quasar.config.js](https://v2.quasar.dev/quasar-cli-vite/quasar-config-js).
+
+### Supabase query
+
+```SQL
+select
+  i.created_at AT TIME ZONE 'Europe/Lisbon' as "criado",
+  i.nome_pessoa,
+  c.nome as "cargo",
+  cb.nome as "clube"
+from
+  inscritos i
+  inner join cargos c on c.id = i.cargo_id
+  inner join clubes cb on cb.id = i.clube_id;
+```
--- src/pages/FormularioSubmetido.vue
+++ src/pages/FormularioSubmetido.vue
@@ -1,13 +1,34 @@
 <template>
   <q-page padding class="flex flex-center">
-    <q-card class="q-pa-xl">
-      <q-card-section class="text-center">
-        <q-icon name="task_alt" color="positive" size="8em" />
-      </q-card-section>
-      <q-card-section>
-        <div class="text-h6">Os dados foram submtidos com sucesso</div>
-      </q-card-section>
-    </q-card>
+    <div class="row justify-center q-ma-xl">
+      <img
+        alt="APA"
+        src="~assets/APA.webp"
+        class="col-12"
+        style="width: 200px"
+      />
+      <img
+        alt="APACA"
+        src="~assets/APA_APACA.jpg"
+        class="col-12"
+        style="width: 200px"
+      />
+
+      <q-card class="q-pa-xl q-ma-xl">
+        <q-card-section class="text-center">
+          <q-icon name="task_alt" color="positive" size="8em" />
+        </q-card-section>
+        <q-card-section>
+          <div class="text-h5 text-center">Agradecemos a sua inscrição.</div>
+          <div class="text-h6 text-center">
+            A validação da sua presença na sessão encontra-se pendente.
+          </div>
+          <div class="text-h6 text-center">
+            A mesma será confirmada durante a próxima semana.
+          </div>
+        </q-card-section>
+      </q-card>
+    </div>
   </q-page>
 </template>
 
