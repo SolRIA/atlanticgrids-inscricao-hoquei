df2c0f53553f9e80583ae7447d71624aa369f242
Frederico Regateiro <frederico.regateiro@solria.pt>
2023-09-17 11:25:47 +0100

    imagens centradas horizontalmente e verticalmente

--- src/pages/FormularioSubmetido.vue
+++ src/pages/FormularioSubmetido.vue
@@ -1,34 +1,33 @@
 <template>
   <q-page padding class="flex flex-center">
-    <div class="row justify-center q-ma-xl">
-      <img
-        alt="APA"
-        src="~assets/APA.webp"
-        class="col-12"
-        style="width: 200px"
-      />
-      <img
-        alt="APACA"
-        src="~assets/APA_APACA.jpg"
-        class="col-12"
-        style="width: 200px"
-      />
-
-      <q-card class="q-pa-xl q-ma-xl">
-        <q-card-section class="text-center">
-          <q-icon name="task_alt" color="positive" size="8em" />
-        </q-card-section>
-        <q-card-section>
-          <div class="text-h5 text-center">Agradecemos a sua inscrição.</div>
-          <div class="text-h6 text-center">
-            A validação da sua presença na sessão encontra-se pendente.
-          </div>
-          <div class="text-h6 text-center">
-            A mesma será confirmada durante a próxima semana.
-          </div>
-        </q-card-section>
-      </q-card>
-    </div>
+    <q-card class="q-pa-xl q-ma-xl">
+      <q-card-section class="text-center">
+        <img
+          alt="APA"
+          src="~assets/APA.webp"
+          class="col-12"
+          style="width: 200px"
+        />
+        <img
+          alt="APACA"
+          src="~assets/APA_APACA.jpg"
+          class="col-12"
+          style="width: 200px"
+        />
+      </q-card-section>
+      <q-card-section class="text-center">
+        <q-icon name="task_alt" color="positive" size="8em" />
+      </q-card-section>
+      <q-card-section>
+        <div class="text-h5 text-center">Agradecemos a sua inscrição.</div>
+        <div class="text-h6 text-center">
+          A validação da sua presença na sessão encontra-se pendente.
+        </div>
+        <div class="text-h6 text-center">
+          A mesma será confirmada durante a próxima semana.
+        </div>
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
